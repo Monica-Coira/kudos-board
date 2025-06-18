9f7d445dee37b92d53fef5af759a60b6aa4c5117
Monica Coira <monicacoira@meta.com>
2025-06-18 10:15:08 -0700

    added CORS

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const PORT = 3000
 const boardRoutes = require('./routes/BoardRoutes')
 const cardRoutes = require('./routes/CardRoutes')
 
 app.use(express.json())
+app.use(cors())
 app.use('/boards', boardRoutes)
 app.use('/cards', cardRoutes)
 
